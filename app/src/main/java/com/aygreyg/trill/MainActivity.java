d91608b11a865694bd3faa5bb95b120698b8d6c0
Dávid Papp <78927537+aygreyG@users.noreply.github.com>
2023-04-15 18:52:00 +0200

    register and login

--- app/src/main/java/com/aygreyg/trill/MainActivity.java
+++ app/src/main/java/com/aygreyg/trill/MainActivity.java
@@ -1,14 +1,104 @@
 package com.aygreyg.trill;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getName();
+    private static final int SECRET_KEY = 11;
+    private static final int RC_SIGN_IN = 123;
+
+    EditText emailEditText;
+    EditText passwordEditText;
+
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailEditText = findViewById(R.id.editTextEmail);
+        passwordEditText = findViewById(R.id.editTextPassword);
+
+        mAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    public void login(View view) {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                Log.d(LOG_TAG, "User successfully logged in! ");
+                Toast.makeText(MainActivity.this, "Successfully logged in! ", Toast.LENGTH_LONG).show();
+            } else {
+                Log.d(LOG_TAG, "User couldn't log in! " + task.getException().getMessage());
+                Toast.makeText(MainActivity.this, "Couldn't log in! " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void openRegister(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        intent.putExtra("SECRET_KEY", SECRET_KEY);
+        startActivity(intent);
+    }
+
+    public void loginWithGoogle(View view) {
+        Intent signInIntent = mGSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                FirebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                Log.e(LOG_TAG, "Google sign in failed", e);
+            }
+        }
+    }
+
+    private void FirebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                Log.d(LOG_TAG, "User successfully logged in! ");
+                Toast.makeText(MainActivity.this, "Successfully logged in! ", Toast.LENGTH_LONG).show();
+            } else {
+                Log.d(LOG_TAG, "User couldn't log in! " + task.getException().getMessage());
+                Toast.makeText(MainActivity.this, "Couldn't log in! " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/aygreyg/trill/RegisterActivity.java
@@ -0,0 +1,112 @@
+package com.aygreyg.trill;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+    private static final String LOG_TAG = RegisterActivity.class.getName();
+    private static final int SECRET_KEY = 11;
+
+    EditText userNameEditText;
+    EditText userEmailEditText;
+    EditText passwordEditText;
+    EditText passwordConfirmEditText;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+        int secret_key = getIntent().getIntExtra("SECRET_KEY", 0);
+
+        if (secret_key != SECRET_KEY) {
+            finish();
+        }
+
+        userNameEditText = findViewById(R.id.editTextUserName);
+        userEmailEditText = findViewById(R.id.editTextEmail);
+        passwordEditText = findViewById(R.id.editTextPassword);
+        passwordConfirmEditText = findViewById(R.id.editTextPasswordAgain);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void cancel(View view) {
+        finish();
+    }
+
+    public void register(View view) {
+        String userName = userNameEditText.getText().toString();
+        String email = userEmailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        if (checkInputs()) {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+                if (task.isSuccessful()) {
+                    Log.d(LOG_TAG, "Registered: " + userName + ", e-mail: " + email);
+                    Toast.makeText(RegisterActivity.this, "User successfully created!", Toast.LENGTH_LONG).show();
+                } else {
+                    Log.d(LOG_TAG, "User couldn't be created! " + task.getException().getMessage());
+                    Toast.makeText(RegisterActivity.this, "User couldn't be created!", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+    private boolean checkInputs() {
+        if (userNameEditText.length() == 0) {
+            userNameEditText.setError("You must provide a username!");
+            return false;
+        }
+
+        if (userEmailEditText.length() == 0) {
+            userEmailEditText.setError("You must provide an email!");
+            return false;
+        }
+
+        if (userEmailEditText.length() == 0) {
+            userEmailEditText.setError("You must provide an email!");
+            return false;
+        }
+
+        String email = userEmailEditText.getText().toString();
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            userEmailEditText.setError("You must provide a valid email!");
+            return false;
+        }
+
+        if (passwordEditText.length() == 0) {
+            passwordEditText.setError("You must provide a password!");
+            return false;
+        }
+
+        if (passwordEditText.getText().toString().length() < 6) {
+            passwordEditText.setError("The password must be at least 6 characters long!");
+            return false;
+        }
+
+        if (passwordConfirmEditText.length() == 0) {
+            passwordConfirmEditText.setError("You must provide the password confirmation!");
+            return false;
+        }
+
+        String password = passwordEditText.getText().toString();
+        String passwordConfirm = passwordConfirmEditText.getText().toString();
+
+        if (!password.equals(passwordConfirm)) {
+            passwordConfirmEditText.setError("The password and confirmation should be the same!");
+            return false;
+        }
+
+        return true;
+    }
+}
